ac0096d54304e697c2a0323799ded9d2bb989932
Gleb Pomykalov <gleb@lancastr.com>
2018-08-08 00:03:24 +0300

    freebsd fixes

--- bpf/src/lib.rs
+++ bpf/src/lib.rs
@@ -54,7 +54,7 @@ pub union ifreq_inner {
     pub ifru_addr: libc::sockaddr,
     pub ifru_dstaddr: libc::sockaddr,
     pub ifru_broadaddr: libc::sockaddr,
-    pub ifru_buffer: libc::ifreq_buffer,
+    pub ifru_buffer: ifreq_buffer,
     pub ifru_flags: [libc::c_short; 2],
     pub ifru_index: libc::c_short,
     pub ifru_jid: libc::c_int,
@@ -62,13 +62,14 @@ pub union ifreq_inner {
     pub ifru_mtu: libc::c_int,
     pub ifru_phys: libc::c_int,
     pub ifru_media: libc::c_int,
-    pub ifru_data: libc::caddr_t,
+    pub ifru_data: caddr_t,
     pub ifru_cap: [libc::c_int; 2],
     pub ifru_fib: libc::c_uint,
     pub ifru_vlan_pcp: libc::c_uchar,
 }
 
 #[repr(C)]
+#[derive(Copy, Clone)]
 pub struct ifreq {
     pub ifr_name: [u8; libc::IFNAMSIZ],
     pub inner: ifreq_inner,
